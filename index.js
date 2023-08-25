48adf2efd7bcd58012e3c00900980972a232ba9b
TuckGem <141772389+TuckGem@users.noreply.github.com>
2023-08-25 15:03:29 +0100

    add link return to home href

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format("h:mm:ss")} 
             <small>${cityTime.format("A")}</small></div>
-        </div> `;
+        </div> 
+        <a href="index.html">All cities</a>`;
 }
 
 let citiesSelectElement = document.querySelector("#city");
